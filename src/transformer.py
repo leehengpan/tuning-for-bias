5890b350ad9096b0f440937b733b62163163a03e
raichandanisagar <raichandanisagar@gmail.com>
2023-05-11 15:52:19 -0400

    Pushing model weight files using LFS

--- src/transformer.py
+++ src/transformer.py
@@ -1,90 +1,28 @@
 import tensorflow as tf
-from encoder import EncoderBlock
-from decoder import DecoderBlock
+from encoder import Encoder
+from decoder import Decoder
 
-# model with multiple encoder blocks and multiple decoder blocks
+
 class TransformerModel(tf.keras.Model):
-    
-    def __init__(self, emb_sz, num_heads, key_dim, vocab_size, **kwargs):
-        
+    def __init__(self, num_layers, num_heads, ff_dim, embedding_size,
+                 content_vocab_size, title_vocab_size, content_window_size, title_window_size,
+                 content_embedding_initializer, title_embedding_initializer,
+                 content_embedding_trainability, title_embedding_trainability,
+                 dropout_rate=0.1):
         super().__init__()
-        
-        # create encoder and decoder blocks from classes 
-        self.encoder_block1 = EncoderBlock(emb_sz,num_heads,key_dim)
-        self.encoder_block2 = EncoderBlock(emb_sz,num_heads,key_dim)
-        # self.encoder_block3 = EncoderBlock(emb_sz,num_heads,key_dim)
-        # self.encoder_block4 = EncoderBlock(emb_sz,num_heads,key_dim)
-        # self.encoder_block5 = EncoderBlock(emb_sz,num_heads,key_dim)
-        # self.encoder_block6 = EncoderBlock(emb_sz,num_heads,key_dim)
-        # self.encoder_block7 = EncoderBlock(emb_sz,num_heads,key_dim)
-        # self.encoder_block8 = EncoderBlock(emb_sz,num_heads,key_dim)
-     
-        self.decoder_block1 = DecoderBlock(emb_sz,num_heads,key_dim)
-        self.decoder_block2 = DecoderBlock(emb_sz,num_heads,key_dim)
-        # self.decoder_block3 = DecoderBlock(emb_sz,num_heads,key_dim)
-        # self.decoder_block4 = DecoderBlock(emb_sz,num_heads,key_dim)
-        # self.decoder_block5 = DecoderBlock(emb_sz,num_heads,key_dim)
-        # self.decoder_block6 = DecoderBlock(emb_sz,num_heads,key_dim)
-        # self.decoder_block7 = DecoderBlock(emb_sz,num_heads,key_dim)
-        # self.decoder_block8 = DecoderBlock(emb_sz,num_heads,key_dim)
-    
-        # dense layer for final output 
-        self.dense_layer = tf.keras.layers.Dense(vocab_size, activation='softmax')
+        self.encoder = Encoder(num_layers, num_heads, ff_dim, content_vocab_size, embedding_size,
+                               content_window_size, content_embedding_initializer, content_embedding_trainability,
+                               dropout_rate)
+        self.decoder = Decoder(num_layers, num_heads, ff_dim, title_vocab_size, embedding_size,
+                               title_window_size, title_embedding_initializer, title_embedding_trainability,
+                               dropout_rate)
 
-    
+        self.dense_layer = tf.keras.layers.Dense(title_vocab_size)
+
     def call(self, inputs):
-
-        # encoder blocks 
-        encoder_output = self.encoder_block1(inputs[0])
-        encoder_output = self.encoder_block2(encoder_output)
-        # encoder_output = self.encoder_block3(encoder_output)
-        # encoder_output = self.encoder_block4(encoder_output)
-        # encoder_output = self.encoder_block5(encoder_output)
-        # encoder_output = self.encoder_block6(encoder_output)
-        # encoder_output = self.encoder_block7(encoder_output)
-        # encoder_output = self.encoder_block8(encoder_output)
-
-        # decoder blocks 
-        decoder_output = self.decoder_block1(encoder_output, inputs[1])
-        decoder_output = self.decoder_block2(encoder_output, decoder_output)
-        # decoder_output = self.decoder_block3(encoder_output, decoder_output)
-        # decoder_output = self.decoder_block4(encoder_output, decoder_output)
-        # decoder_output = self.decoder_block5(encoder_output, decoder_output)
-        # decoder_output = self.decoder_block6(encoder_output, decoder_output)
-        # decoder_output = self.decoder_block7(encoder_output, decoder_output)
-        # decoder_output = self.decoder_block8(encoder_output, decoder_output)
-
-        # final logit outputs 
-        logits = self.dense_layer(decoder_output)
-        
-        
+        content, title = inputs
+        print('At transformer call: ', content==None, title==None)
+        context = self.encoder(content)
+        output = self.decoder(title, context)
+        logits = self.dense_layer(output)
         return logits
-    
-    def masked_loss(label, pred):
-        mask = label != 0
-        loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False, reduction='none')
-        loss = tf.expand_dims(loss_object(label, pred), axis=2)
-
-        mask = tf.cast(mask, dtype=loss.dtype)
-        loss *= mask
-
-        loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
-        return loss
-
-    def masked_accuracy(label, pred):
-        pred = tf.expand_dims(tf.argmax(pred, axis=2), axis=2)
-        label = tf.cast(label, pred.dtype)
-        match = label == pred
-
-        mask = label != 0
-
-        match = match & mask
-
-        match = tf.cast(match, dtype=tf.float32)
-        mask = tf.cast(mask, dtype=tf.float32)
-        return tf.reduce_sum(match)/tf.reduce_sum(mask)
-    
-    def get_config(self):
-        return {'encoder_block1': self.encoder_block1,'encoder_block2': self.encoder_block2, 
-                'decoder_block1': self.decoder_block1, 'decoder_block2': self.decoder_block2, 
-                'dense_layer': self.dense_layer}
